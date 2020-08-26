ef6032f7a2f7f995f546c1f089043df6166dc1b9
Kaitlyn Nguyen <knguye42@xula.edu>
2020-08-26 17:47:46 +0000

    prints a message

---
+++ Greeter.java
@@ -0,0 +1,9 @@
+public class Greeter {
+   public static void main(String[] args) {
+     if (args.length > 0){
+       System.out.printf("Greetings, %s\n",args[0]);
+       } else {
+       System.out.printf("Greetings, earthinling!"):
+     }
+   }
+}
